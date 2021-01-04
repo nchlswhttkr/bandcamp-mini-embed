2b1926f8d0723811cd6dd560fcdc0e532dc031b9
Nicholas Whittaker <nicholas@nicholas.cloud>
2021-01-04 17:46:13 +1100

    Add controls, styles

--- README.md
+++ README.md
@@ -8,16 +8,19 @@ npm run dev
 npm run build
 ```
 
+Bandcamp logo is property of [Bandcamp](https://bandcamp.com). Other icons belong to [Feather](https://feathericons.com/).
+
 <!--
 TODO
  - https://botany.bandcamp.com/track/fourteen-45-tails
  - Support tracks rather than only albums
  - Handling play suspending (waiting)
- - Show currently playing track in addition to album/artist
- - Show current track's artist rather than album's artist
  - Document that player doesn't seem to play nice with constructor API
  - Check compatability with audio/source elements, console errors
  - List tracks in albums, allow streamble songs to be selected
- - Fix accessibility warnings, check for guidance
+ - Fix accessibility warnings, check for guidance on range input
  - Make origin customisable
+ - Error handling if no tracks are streamable
+ - BUG: Double click play button
+ - BUG: Jumpy range slider when playing new track
  -->
--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,30 @@
 <script>
   export let albumId;
 
+  import bandcampLogo from "./icons/bandcamp-logotype-color.png";
+  import playIcon from "./icons/play.svg";
+  import pauseIcon from "./icons/pause.svg";
+  import previousIcon from "./icons/previous.svg";
+  import nextIcon from "./icons/next.svg";
+
   let tracks;
+  let startingTrack;
   let previousTrack;
   let currentTrack;
   let nextTrack;
   let artist;
   let album;
+  let albumUrl;
   let artwork;
-  let startingTrack = 0;
   let audio;
   let paused;
+  let currentTime;
 
   $: {
     // Update currently playing audio as track changes
     if (audio !== undefined) {
       if (currentTrack !== undefined) {
         audio.src = tracks[currentTrack].file["mp3-128"];
-      } else {
-        audio.src = "";
       }
     }
   }
@@ -64,6 +70,7 @@
     artwork = response.album_art;
     artist = response.artist;
     album = response.album_title;
+    albumUrl = response.linkback + "?from=embed";
     tracks = response.tracks;
     startingTrack =
       tracks.findIndex(track => track.id === response.featured_track_id) || 0;
@@ -82,36 +89,188 @@
   function pause() {
     audio.pause();
   }
+
+  function toggle() {
+    if (currentTrack === undefined) {
+      play(startingTrack);
+    } else if (paused) {
+      play(currentTrack);
+    } else {
+      pause();
+    }
+  }
+
+  let seekingTime;
+  function handleSeeking(event) {
+    switch (event.type) {
+      case "change":
+        currentTime = event.target.value;
+        seekingTime = undefined;
+        break;
+      case "input":
+        seekingTime = event.target.value;
+        break;
+      default:
+        break;
+    }
+  }
 </script>
 
-{#await load()}
-  <p>Loading...</p>
-{:then _}
-  <div>
-    <img src={artwork} alt="Cover artwork for {album}, by {artist}" />
-    <audio
-      bind:this={audio}
-      bind:paused
-      on:ended={() => play(nextTrack)}
-      autoplay="true" />
-    <p>{album} - {artist}</p>
-    {#if paused || currentTrack === undefined}
-      <button
-        on:click={() => play(currentTrack === undefined ? startingTrack : currentTrack)}>
-        Play
-      </button>
-    {:else}
-      <button on:click={pause}>Pause</button>
-    {/if}
-    <button
-      on:click={() => play(previousTrack)}
-      disabled={previousTrack === undefined}>
-      Previous
-    </button>
-    <button on:click={() => play(nextTrack)} disabled={nextTrack === undefined}>
-      Next
-    </button>
-  </div>
-{:catch error}
-  <p style="color: red">{error}</p>
-{/await}
+<style>
+  button {
+    color: #555;
+    border: none;
+    background-color: #fff;
+    padding: 4px;
+    line-height: 0;
+  }
+  button:hover {
+    cursor: pointer;
+    color: #111;
+  }
+  button:disabled {
+    cursor: auto;
+    color: #bbb;
+  }
+
+  .bandcamp-mini-embed {
+    font-family: sans-serif;
+    max-width: 480px;
+    border: 1px solid #bbb;
+  }
+  a {
+    color: #61929c;
+    font-weight: 700;
+  }
+
+  .player {
+    display: flex;
+    height: 120px;
+  }
+
+  .info {
+    flex-shrink: 1;
+    padding: 8px 0 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    overflow: hidden;
+  }
+  .info p {
+    margin: 0 8px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .controls {
+    width: 100%;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    padding-right: 4px; /* line up with links below */
+  }
+  .controls > *:first-child {
+    padding-left: 8px; /* keep space between play button/artwork interactive */
+  }
+  .controls > input {
+    flex-grow: 1;
+    width: 0; /* force smaller width on Firefox */
+  }
+
+  .artwork {
+    cursor: pointer;
+    flex-grow: 0;
+  }
+
+  /* Links along bottom, Bandcamp logo to far right */
+  .links {
+    box-sizing: border-box;
+    padding: 8px 0 8px 8px;
+    display: flex;
+    align-items: center;
+  }
+  .links > * {
+    margin-right: 16px;
+  }
+  .links > .spacer {
+    flex-grow: 1;
+  }
+  .links > *:last-child {
+    line-height: 0;
+    margin-right: 0;
+  }
+</style>
+
+<audio
+  bind:this={audio}
+  bind:paused
+  bind:currentTime
+  on:ended={() => play(nextTrack)}
+  autoplay="on" />
+<div class="bandcamp-mini-embed">
+  {#await load()}
+    <p>Loading...</p>
+  {:then _}
+    <div class="player">
+      <img
+        height="120"
+        width="120"
+        class="artwork"
+        on:click={toggle}
+        src={artwork}
+        alt="Cover artwork for {album}, by {artist}" />
+      <div class="info">
+        <a href={albumUrl}>
+          <p>
+            {tracks[currentTrack === undefined ? startingTrack : currentTrack].title}
+          </p>
+        </a>
+        <p>
+          {tracks[currentTrack === undefined ? startingTrack : currentTrack].artist}
+        </p>
+        <p>{album}</p>
+        <div class="controls">
+          {#if paused || currentTrack === undefined}
+            <button
+              on:click={() => play(currentTrack === undefined ? startingTrack : currentTrack)}>
+              {@html playIcon}
+            </button>
+          {:else}
+            <button on:click={pause}>
+              {@html pauseIcon}
+            </button>
+          {/if}
+          <input
+            type="range"
+            min="0"
+            max={currentTrack === undefined ? undefined : Math.floor(tracks[currentTrack].duration)}
+            value={(seekingTime === undefined ? currentTime : seekingTime) || 0}
+            on:change={handleSeeking}
+            on:input={handleSeeking} />
+          <button
+            on:click={() => play(previousTrack)}
+            disabled={previousTrack === undefined}>
+            {@html previousIcon}
+          </button>
+          <button
+            on:click={() => play(nextTrack)}
+            disabled={nextTrack === undefined}>
+            {@html nextIcon}
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="links">
+      <a href={`${albumUrl}&action=buy`}>buy</a>
+      <a href={`${albumUrl}&action=share`}>share</a>
+      <div class="spacer" />
+      <a href={albumUrl}>
+        <img height="32" width="110" src={bandcampLogo} alt="Bandcamp logo" />
+      </a>
+    </div>
+  {:catch error}
+    <p style="color: red">{error}</p>
+  {/await}
+</div>
