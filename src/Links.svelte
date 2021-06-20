5e4cfe0e37445d55a1e05cf8ef9d49cb1ea23a7b
Nicholas Whittaker <nicholas@nicholas.cloud>
2021-06-20 16:27:17 +1000

    Break off links into a separate component

---
+++ src/Links.svelte
@@ -0,0 +1,51 @@
+<script>
+  export let albumUrl;
+
+  import bandcampLogoColor from "./icons/bandcamp-logotype-color.png";
+  import bandcampLogoWhite from "./icons/bandcamp-logotype-white.png";
+</script>
+
+<div class="links">
+  <a href={`${albumUrl}&action=buy`}>buy</a>
+  &nbsp;
+  <a href={`${albumUrl}&action=share`}>share</a>
+  <a class="logo" href={albumUrl}>
+    <picture>
+      <source srcset={bandcampLogoWhite} media="(prefers-color-scheme: dark)" />
+      <img src={bandcampLogoColor} alt="Bandcamp logo" />
+    </picture>
+  </a>
+</div>
+
+<style>
+  a {
+    color: #537d86;
+    text-decoration: none;
+    font-weight: 700;
+  }
+  @media (prefers-color-scheme: dark) {
+    a {
+      color: inherit;
+    }
+  }
+
+  .links {
+    height: 46px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+  .links > *:first-child {
+    margin-right: 8px; /* fix space between buy/share links */
+  }
+
+  .links > .logo {
+    line-height: 0;
+  }
+
+  .links > .logo > picture > img {
+    height: 32px;
+    width: 110px;
+  }
+</style>
