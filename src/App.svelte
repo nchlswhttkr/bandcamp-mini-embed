771b9cd11baf90a570da01db61bec3af0e044253
Nicholas Whittaker <nicholas@nicholas.cloud>
2021-06-20 17:02:33 +1000

    Break off player into separate component

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,9 @@
   export let fallbackText;
   export let fallbackUrl;
 
-  import playIcon from "./icons/play.svg";
-  import pauseIcon from "./icons/pause.svg";
-  import previousIcon from "./icons/previous.svg";
-  import nextIcon from "./icons/next.svg";
-
   import Links from "./Links.svelte";
   import Tracklist from "./Tracklist.svelte";
+  import Player from "./Player.svelte";
 
   let tracks;
   let startingTrack;
@@ -154,66 +150,23 @@
 />
 <div class="root">
   {#await load() then _}
-    <div class="player">
-      <div class="artwork">
-        <img on:click={toggle} src={artwork} alt="Cover artwork for {album}" />
-      </div>
-      <div class="info">
-        <p>
-          <a href={albumUrl}>
-            {@html tracks[
-              currentTrack === undefined ? startingTrack : currentTrack
-            ].title}
-          </a>
-        </p>
-        <p>
-          {@html tracks[
-            currentTrack === undefined ? startingTrack : currentTrack
-          ].artist}
-        </p>
-        <p>{@html album}</p>
-        <div class="controls">
-          {#if paused || currentTrack === undefined}
-            <button
-              aria-label="Play current song"
-              on:click={() =>
-                play(currentTrack === undefined ? startingTrack : currentTrack)}
-            >
-              {@html playIcon}
-            </button>
-          {:else}
-            <button aria-label="Pause current song" on:click={pause}>
-              {@html pauseIcon}
-            </button>
-          {/if}
-          <input
-            aria-label="Seek"
-            type="range"
-            min="0"
-            max={currentTrack === undefined
-              ? undefined
-              : Math.floor(tracks[currentTrack].duration)}
-            value={(seekingTime === undefined ? currentTime : seekingTime) || 0}
-            on:change={handleSeeking}
-            on:input={handleSeeking}
-          />
-          <button
-            aria-label="Play previous song"
-            on:click={() => play(previousTrack)}
-            disabled={previousTrack === undefined}
-          >
-            {@html previousIcon}
-          </button>
-          <button
-            aria-label="Play next song"
-            on:click={() => play(nextTrack)}
-            disabled={nextTrack === undefined}
-          >
-            {@html nextIcon}
-          </button>
-        </div>
-      </div>
-    </div>
+    <Player
+      {toggle}
+      {artwork}
+      {album}
+      {albumUrl}
+      {tracks}
+      {currentTrack}
+      {startingTrack}
+      {paused}
+      {play}
+      {pause}
+      {seekingTime}
+      {currentTime}
+      {handleSeeking}
+      {previousTrack}
+      {nextTrack}
+    />
     <Tracklist {tracks} {currentTrack} {play} {artist} />
     <Links {albumUrl} />
   {:catch _}
@@ -240,86 +193,5 @@
   /* This interferes the SVGs inside buttons, ignore them */
   .root :global(:not(button *)) {
     all: revert;
-  }
-
-  button {
-    cursor: pointer;
-    opacity: 0.8;
-    color: inherit;
-    border: none;
-    background-color: unset;
-    padding: 4px;
-    line-height: 0;
-  }
-  button:hover {
-    opacity: 1;
-  }
-  button:disabled {
-    cursor: auto;
-    opacity: 0.5;
-  }
-  a {
-    color: #537d86;
-    text-decoration: underline;
-    font-weight: 700;
-  }
-  @media (prefers-color-scheme: dark) {
-    a {
-      color: inherit;
-    }
-  }
-
-  .player {
-    display: flex;
-    height: 120px;
-  }
-
-  .info {
-    flex: 1 1 0;
-    min-width: 198px;
-    padding: 12px 0 0;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    overflow: hidden;
-  }
-  .info p {
-    margin: 0 8px 0 12px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-  .info > p:last-of-type {
-    font-style: italic;
-  }
-
-  .controls {
-    width: 100%;
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    padding-right: 4px; /* line up with links below */
-  }
-  .controls > *:first-child {
-    padding-left: 8px; /* keep space between play button/artwork interactive */
-  }
-  .controls > input {
-    cursor: grab;
-    flex-grow: 1;
-    width: 0; /* force smaller width on Firefox */
-  }
-  .controls > input:active {
-    cursor: grabbing;
-  }
-
-  .artwork {
-    cursor: pointer;
-    height: 120px;
-    width: 120px;
-    min-width: 80px;
-    display: flex;
-    justify-content: center;
-    overflow: hidden;
   }
 </style>
