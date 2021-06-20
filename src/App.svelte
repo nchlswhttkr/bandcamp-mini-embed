d663eebe82eefd791306ca31f8e71e79e4ffa335
Nicholas Whittaker <nicholas@nicholas.cloud>
2021-06-20 16:55:04 +1000

    Break off tracklist into a separate component

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,7 @@
   import nextIcon from "./icons/next.svg";
 
   import Links from "./Links.svelte";
+  import Tracklist from "./Tracklist.svelte";
 
   let tracks;
   let startingTrack;
@@ -213,32 +214,7 @@
         </div>
       </div>
     </div>
-    <ul class="tracks">
-      {#each tracks as track, i}
-        <li
-          tabindex={tracks[i].track_streaming ? 0 : -1}
-          class:now-playing={i === currentTrack}
-          class:unstreamable={!tracks[i].track_streaming}
-          on:click={() => tracks[i].track_streaming && play(i)}
-          on:keydown={(e) => {
-            if (tracks[i].track_streaming && e.key === " ") {
-              e.preventDefault();
-              play(i);
-            }
-          }}
-        >
-          <span>
-            {Math.floor(track.duration / 60)
-              .toString()
-              .padStart(2, " ")}:{Math.floor(track.duration % 60)
-              .toString()
-              .padStart(2, "0")}
-          </span>
-          {@html "  " + track.title}
-          {#if track.artist !== artist}– {track.artist}{/if}
-        </li>
-      {/each}
-    </ul>
+    <Tracklist {tracks} {currentTrack} {play} {artist} />
     <Links {albumUrl} />
   {:catch _}
     {#if fallbackText && fallbackUrl}
@@ -345,33 +321,5 @@
     display: flex;
     justify-content: center;
     overflow: hidden;
-  }
-
-  .tracks {
-    margin: 8px 0 0;
-    padding: 0;
-    height: 160px;
-    overflow-y: scroll;
-  }
-  .tracks > * {
-    font-size: 12px;
-    cursor: pointer;
-    margin: 0px 12px;
-    padding: 10px 8px;
-    list-style-type: none;
-    border-bottom: 1px solid #bbb;
-    white-space: pre;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-  .tracks > .now-playing {
-    font-weight: 700;
-  }
-  .tracks > .unstreamable {
-    cursor: default;
-    opacity: 0.5;
-  }
-  .tracks span {
-    font-family: monospace;
   }
 </style>
---
+++ src/Tracklist.svelte
@@ -0,0 +1,63 @@
+<script>
+  export let tracks;
+  export let currentTrack;
+  export let play;
+  export let artist;
+</script>
+
+<ul class="tracks">
+  {#each tracks as track, i}
+    <li
+      tabindex={tracks[i].track_streaming ? 0 : -1}
+      class:now-playing={i === currentTrack}
+      class:unstreamable={!tracks[i].track_streaming}
+      on:click={() => tracks[i].track_streaming && play(i)}
+      on:keydown={(e) => {
+        if (tracks[i].track_streaming && e.key === " ") {
+          e.preventDefault();
+          play(i);
+        }
+      }}
+    >
+      <span>
+        {Math.floor(track.duration / 60)
+          .toString()
+          .padStart(2, " ")}:{Math.floor(track.duration % 60)
+          .toString()
+          .padStart(2, "0")}
+      </span>
+      {@html "  " + track.title}
+      {#if track.artist !== artist}– {track.artist}{/if}
+    </li>
+  {/each}
+</ul>
+
+<style>
+  .tracks {
+    margin: 8px 0 0;
+    padding: 0;
+    height: 160px;
+    overflow-y: scroll;
+  }
+  .tracks > * {
+    font-size: 12px;
+    cursor: pointer;
+    margin: 0px 12px;
+    padding: 10px 8px;
+    list-style-type: none;
+    border-bottom: 1px solid #bbb;
+    white-space: pre;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .tracks > .now-playing {
+    font-weight: 700;
+  }
+  .tracks > .unstreamable {
+    cursor: default;
+    opacity: 0.5;
+  }
+  .tracks span {
+    font-family: monospace;
+  }
+</style>
